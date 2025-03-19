71cc600a97d9eb5014e69adbbc69a48e6e854011
ismail-hasan-tanjer <tanjerinfo@gmail.com>
2025-03-19 23:58:27 +0600

    Problem: FizzBuzz

--- Problem-Solving-With-Python.py
+++ Problem-Solving-With-Python.py
@@ -7,5 +7,24 @@ If the number is divisible by 5, then print "Buzz".
 If it is divisible by both 3 and 5, then print "FizzBuzz".
 Otherwise, print the number itself.'''
 
+#solutions
+
+#code 
+
+def fizzbuzz(n):
+    for i in range(1, n+1):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
+# test run 
+fizzbuzz(15)
+
+
 
 
