1069268067f2092575e727cb0f8a26cb67b6e304
ismail-hasan-tanjer <tanjerinfo@gmail.com>
2025-03-19 23:53:36 +0600

    Problem Solving with Python

---
+++ Problem-Solving-With-Python.py
@@ -0,0 +1,11 @@
+'''🔥 Programming Problem Solving Questions and Solutions
+1️⃣ Problem: FizzBuzz
+Description: For numbers from 1 to n—
+
+If the number is divisible by 3, then print "Fizz".
+If the number is divisible by 5, then print "Buzz".
+If it is divisible by both 3 and 5, then print "FizzBuzz".
+Otherwise, print the number itself.'''
+
+
+
