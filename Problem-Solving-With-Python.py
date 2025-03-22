f8d35f16c42f80986818bb565205e8428bbb30cb
ismail-hasan-tanjer <tanjerinfo@gmail.com>
2025-03-22 10:42:40 +0600

    code of amrstrong number

--- Problem-Solving-With-Python.py
+++ Problem-Solving-With-Python.py
@@ -116,4 +116,13 @@ print(sum_of_digits(9876))   # Output: 30
 
     """
     
+    def is_armstrong(n):
+        num_str = str (n)
+        power = len(num_str)
+        return n == sum(int(digit) ** power for digit is num_str)
+    
+    print(is_armstrong(153))
+    print(is_armstrong(9474))
+    print(is_armstrong(123))
+    
     
