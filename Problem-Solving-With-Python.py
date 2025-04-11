079d2c5dc4ff56af76e2db563135de3f4368642c
ismail-hasan-tanjer <tanjerinfo@gmail.com>
2025-04-11 15:29:24 +0600

    Detect Palindrome Word 1

--- Problem-Solving-With-Python.py
+++ Problem-Solving-With-Python.py
@@ -1224,3 +1224,10 @@ def is_palindrome(word):
     return word == word[::-1]
 
 print(is_palindrome("madam"))
+
+
+#Count Vowels in a String
+
+def count_vowels(text):
+    vowels = 'aeiouAEIOU'
+    return sum(1 for char in text if char in vowels)
