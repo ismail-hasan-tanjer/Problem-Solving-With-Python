5c07446a7e63273a916533b939f1d35d96e6bac0
ismail-hasan-tanjer <tanjerinfo@gmail.com>
2025-04-19 23:57:10 +0600

    code of countdown timer 2

--- Problem-Solving-With-Python.py
+++ Problem-Solving-With-Python.py
@@ -1343,7 +1343,12 @@ import time
 
 def countdown(seconds):
     while seconds:
-mins, secs = divmod(seconds, 60)
+        mins, secs = divmod(seconds, 60)
         print(f"{mins:02}:{secs:02}", end='\r')
         time.sleep(1)
         seconds -= 1
+        print("Time's up!")
+
+countdown(5)
+
+
