3db8ca858de3327b5ced4f19e520db0e67110a46
ismail-hasan-tanjer <tanjerinfo@gmail.com>
2025-04-22 08:59:11 +0600

    Leap Year Checker 2

--- Problem-Solving-With-Python.py
+++ Problem-Solving-With-Python.py
@@ -1437,3 +1437,7 @@ generate_marksheet("Tanjer", {"Math": 80, "English": 75, "ICT": 90})
 # Leap Year Checker
 
 def is_leap_year(year):
+    return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
+print(is_leap_year(2024))
+
+
